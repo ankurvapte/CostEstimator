e505be0bf2811362e711ec108d688528661bde8f
Ankur Apte <ankurapte@outlook.com>
2024-07-17 23:16:27 +0530

    Created Employee class

---
+++ Models/Employee.cs
@@ -0,0 +1,10 @@
+﻿namespace CostEstimator;
+
+public class Employee
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Department { get; set; }
+    public string Email { get; set; }
+
+}
