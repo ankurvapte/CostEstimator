132b621d5b17ac626145035964722935bcb33398
Ankur Apte <ankurapte@outlook.com>
2024-07-19 01:14:31 +0530

    Added DbContext file in a namespace

--- Data/CostEstimatorDataContext.cs
+++ Data/CostEstimatorDataContext.cs
@@ -1,6 +1,7 @@
 using CostEstimator;
 using Microsoft.EntityFrameworkCore;
 
+namespace CostEstimator.Data;
 public class CostEstimatorDataContext : DbContext
 {
     public CostEstimatorDataContext(DbContextOptions<CostEstimatorDataContext> options)
