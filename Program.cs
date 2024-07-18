271ae4f2b6ddcc15940445492531b67d7e62efd3
Ankur Apte <ankurapte@outlook.com>
2024-07-19 00:59:39 +0530

    1. Added default connecting string tag; not the connection string 2. Added the middleware for DbContext

--- Program.cs
+++ Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -12,6 +14,10 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+
+builder.Services.AddDbContext<CostEstimatorDataContext>(options=>{
+    options.UseMySQL(builder.Configuration.GetConnectionString(""))
+});
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
