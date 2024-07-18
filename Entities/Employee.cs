f8c85e0c76d60762f742a0b588b542fac2542963
Ankur Apte <ankurapte@outlook.com>
2024-07-19 00:42:14 +0530

    1. Created Employee Entity class 2. Introduced a .gitignore file

---
+++ Entities/Employee.cs
@@ -0,0 +1,9 @@
+﻿namespace CostEstimator;
+
+public class Employee
+{
+    public int Id { get; set; }
+    public required string Name { get; set; }
+    public string Department { get; set; } = string.Empty;
+    public required string Email { get; set; }
+}
